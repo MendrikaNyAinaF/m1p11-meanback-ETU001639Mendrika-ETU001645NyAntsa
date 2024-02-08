896011bd1265ad38d647388271559d381891b0ab
nyantsaranja <99872725+nyantsaranja@users.noreply.github.com>
2024-02-08 09:12:14 +0300

    documentation

--- config.js
+++ config.js
@@ -3,12 +3,14 @@
 const config = {
   development: {
     mongoURI: 'mongodb+srv://ranjalahynyantsa:c7jt3yoVjNP6vqq6@cluster0.cdwza34.mongodb.net/',
-    dbName: 'mbs-dev'
+    dbName: 'mbs-dev',
+    secretKey:'secretKeyDev2024!'
     // Add other development-specific configuration here
   },
   production: {
     mongoURI: 'mongodb+srv://ranjalahynyantsa:c7jt3yoVjNP6vqq6@cluster0.cdwza34.mongodb.net/',
-    dbName: 'mbs-prod'
+    dbName: 'mbs-prod',
+    secretKey:'secretKeyProd2024!'
     // Add other production-specific configuration here
   }
 };
