09ce2ded628dd2945bc89d863eeaa6bf36862922
nyantsaranja <99872725+nyantsaranja@users.noreply.github.com>
2024-02-11 10:09:45 +0300

    new db connection strings

--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 
 const config = {
   development: {
-    mongoURI: 'mongodb+srv://ranjalahynyantsa:c7jt3yoVjNP6vqq6@cluster0.cdwza34.mongodb.net/',
+    mongoURI: 'mongodb+srv://ranjalahynyantsa:y6SEIUJTvmJlJx0y@cluster0.cdwza34.mongodb.net/',
     dbName: 'mbs-dev',
     secretKey:'secretKeyDev2024!'
     // Add other development-specific configuration here
   },
   production: {
-    mongoURI: 'mongodb+srv://ranjalahynyantsa:c7jt3yoVjNP6vqq6@cluster0.cdwza34.mongodb.net/',
+    mongoURI: 'mongodb+srv://ranjalahynyantsa:y6SEIUJTvmJlJx0y@cluster0.cdwza34.mongodb.net/',
     dbName: 'mbs-prod',
     secretKey:'secretKeyProd2024!'
     // Add other production-specific configuration here
