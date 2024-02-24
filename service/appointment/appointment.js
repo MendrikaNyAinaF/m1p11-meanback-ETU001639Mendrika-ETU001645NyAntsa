0a50124be878602d14569a2e57c9f16369e22276
nyantsaranja <99872725+nyantsaranja@users.noreply.github.com>
2024-02-24 22:08:35 +0300

    correct framework so it takes all related childs automatically

--- service/appointment/appointment.js
+++ service/appointment/appointment.js
@@ -271,10 +271,12 @@ const findAllAppointmentForEmployee = async (req, res) => {
         }
     }
 
+    const referencedObject = await crud.addObjectReferenced(rendez_vous_details, req.db)
+
     res.send({
             code: 200,
             message: "Appointments found",
-            data: rendez_vous_details
+            data: referencedObject
         }
     )
 }
