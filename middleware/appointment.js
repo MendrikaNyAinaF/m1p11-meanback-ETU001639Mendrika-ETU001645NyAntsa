6f45e5ba5758eef5fb9dafae8452eb4a29c8b06f
nyantsaranja <99872725+nyantsaranja@users.noreply.github.com>
2024-02-24 21:18:32 +0300

    dynamic criteria management

--- middleware/appointment.js
+++ middleware/appointment.js
@@ -1,10 +1,18 @@
 const {ObjectId} = require("mongodb");
 const addStatutAppointmentMiddlewareForClient = async (req, res, next) => {
-    req.body.search['$and'].push({
-        status :{
+// get criteria, parse it from base64 and the above property and update the criteria in request
+    let criteria = req.query.criteria
+    try {
+        criteria = JSON.parse(atob(criteria));
+    } catch (e) {
+        criteria = req.body
+    }
+    criteria.search['$and'].push({
+        status: {
             "$ne": new ObjectId("65c23d5d3fe8b2bd4b8f7e0c")
         }
     })
+    req.query.criteria = btoa(JSON.stringify(criteria))
 //     call next
     next()
 }
